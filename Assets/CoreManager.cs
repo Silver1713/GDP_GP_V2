907c35a21b04b8f5a695cc370aa25adca5ad6bc5
Yeo Jia Hao <yeojiahao89@gmail.com>
2019-12-05 04:51:37 +0800

    Final Commit

---
+++ Assets/CoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreManager : MonoBehaviour
+{
+    //Core/Data/Game Manager should be a singleton
+    //They should not have more then one instances
+
+    public static CoreManager mainframe;
+    private int CharacterState;
+    private void Awake()
+        //Making it a singleton class
+    {
+        if (mainframe == null)
+        {
+            mainframe = this;
+        }
+        else if (mainframe != this) Destroy(gameObject);
+    }
+
+    public string getCharState()
+    {
+        if (CharacterState == 0)
+        {
+            return "Male";
+        } else if (CharacterState == 1)
+        {
+            return "Female";
+        } else
+        {
+            return null;
+        }
+    }
+
+    public void setCharState(int characterState)
+    {
+        this.CharacterState = characterState;
+    }
+
+
+    
+
+
+}
---
+++ Assets/Scripts/Jia Hao/Essential/SetEssential.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetEssential : MonoBehaviour
+{
+    ///<summary>
+    /// Allow game object to not be destroy when changing scences
+    /// This scripts is only applied to essential items like the mainframe of the game  
+    /// </summary>
+    /// 
+    [Header("Essential Controls")]
+    [Tooltip("Set if an objecct is essential and keep them when changing scene")] public bool Essential;
+
+
+    private void Start()
+    {
+        if (Essential)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}

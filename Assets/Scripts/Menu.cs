354e62a3b6d287793cc0e874ba7902a4f64f0ee7
Yeo Jia Hao <yeojiahao89@gmail.com>
2019-12-05 04:20:38 +0800

    Final Commit for today

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject currentScreen;
+    public GameObject nextScreen;
+    public CoreManager core;
 
     public void playgame()
     {
+        core = CoreManager.mainframe;
+        core.setCharState(0); //Male
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void navigateScreen()
+    {
+        if (nextScreen)
+        {
+            nextScreen.SetActive(true);
+            currentScreen.SetActive(false);
+        }
     }
     public void end()
     {
+#if (UNITY_EDITOR)
+        UnityEditor.EditorApplication.isPlaying = false;
+        
+
+#endif
         Application.Quit();
     }
 }
